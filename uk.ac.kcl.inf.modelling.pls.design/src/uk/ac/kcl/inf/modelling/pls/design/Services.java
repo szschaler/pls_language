f0d14147ab124ae57308ed7569d237cbc7b2b158
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2018-07-16 15:03:42 +0100

    Ensure animation can update the right layers

--- uk.ac.kcl.inf.modelling.pls.design/src/uk/ac/kcl/inf/modelling/pls/design/Services.java
+++ uk.ac.kcl.inf.modelling.pls.design/src/uk/ac/kcl/inf/modelling/pls/design/Services.java
@@ -1,11 +1,15 @@
 package uk.ac.kcl.inf.modelling.pls.design;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gemoc.executionframework.extensions.sirius.services.AbstractGemocAnimatorServices;
 
 /**
  * The services class used by VSM.
  */
-public class Services {
+public class Services extends AbstractGemocAnimatorServices  {
     
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
@@ -14,4 +18,11 @@ public class Services {
        // TODO Auto-generated code
       return self;
     }
+
+	@Override
+	protected List<StringCouple> getRepresentationRefreshList() {
+		final List<StringCouple> res = new ArrayList<StringCouple>();
+		res.add(new StringCouple("Production Line Diagram", "Default"));
+		return res;
+	}
 }
